da33a857f032ef02ad74f47c6067ce69d0dd8379
sayed atef <sayedatef6293@gmail.com>
2023-04-24 23:04:27 +0000

    another modification

--- handle_print.c
+++ handle_print.c
@@ -1,46 +1,41 @@
 #include "main.h"
+
 /**
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
+ * @ind: Pointer to the current index of fmt string.
  * @list: List of arguments to be printed.
- * @ind: ind.
  * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: 1 or 2;
+ *
+ * Return: Number of characters printed, or -1 on failure.
  */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-	int flags, int width, int precision, int size)
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[])
 {
-	int i, unknow_len = 0, printed_chars = -1;
-	fmt_t fmt_types[] = {
-		{'c', print_char}, {'s', print_string}, {'%', print_percent},
-		{'i', print_int}, {'d', print_int}, {'\0', NULL}
-	};
-	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+    int i, unknown_len = 0, printed_chars = -1;
+    fmt_t fmt_types[] = {
+        {'c', print_char}, {'s', print_string}, {'%', print_percent}, {'i', print_int}, {'d', print_int}, {'\0', NULL}};
+    for (i = 0; fmt_types[i].fmt != '\0'; i++)
+        if (fmt[*ind] == fmt_types[i].fmt)
+            return (fmt_types[i].fn(list, buffer));
 
-	if (fmt_types[i].fmt == '\0')
-	{
-		if (fmt[*ind] == '\0')
-			return (-1);
-		unknow_len += write(1, "%%", 1);
-		if (fmt[*ind - 1] == ' ')
-			unknow_len += write(1, " ", 1);
-		else if (width)
-		{
-			--(*ind);
-			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-				--(*ind);
-			if (fmt[*ind] == ' ')
-				--(*ind);
-			return (1);
-		}
-		unknow_len += write(1, &fmt[*ind], 1);
-		return (unknow_len);
-	}
-	return (printed_chars);
+    if (fmt_types[i].fmt == '\0')
+    {
+        if (fmt[*ind] == '\0')
+            return (-1);
+        unknown_len += write(1, "%%", 1);
+        if (fmt[*ind - 1] == ' ')
+            unknown_len += write(1, " ", 1);
+        else if (*ind > 0)
+        {
+            --(*ind);
+            while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+                --(*ind);
+            if (fmt[*ind] == ' ')
+                --(*ind);
+            return (1);
+        }
+        unknown_len += write(1, &fmt[*ind], 1);
+        return (unknown_len);
+    }
+    return (printed_chars);
 }
