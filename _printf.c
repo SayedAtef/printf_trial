02a0d1bb154135e60a1302d176524ce4bb79404b
sayed atef <sayedatef6293@gmail.com>
2023-04-24 13:01:30 +0000

    ads

--- _printf.c
+++ _printf.c
@@ -1,56 +1,70 @@
 #include "main.h"
 
 /**
+ * _print_string - Prints a string to stdout and returns the number of characters printed
+ * @s: The string to print
+ * Return: The number of characters printed
+ */
+int _print_string(char *s)
+{
+    int printed_chars = 0;
+    if (s == NULL)
+        s = "(null)";
+    while (*s)
+    {
+        putchar(*s);
+        s++;
+        printed_chars++;
+    }
+    return printed_chars;
+}
+
+/**
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	int printed_chars = 0;
-	va_list list;
+    int printed_chars = 0;
+    va_list list;
 
-	if (format == NULL)
-		return (-1);
-	va_start(list, format);
-	while (*format)
-	{
-		if (*format != '%')
-		{
-			putchar(*format);
-			printed_chars++;
-		}
-		else
-		{
-			format++;
-			if (*format == 'c')
-			{
-				char c = va_arg(list, int);
+    if (format == NULL)
+        return (-1);
 
-				putchar(c);
-				printed_chars++;
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(list, char *);
+    va_start(list, format);
 
-				if (s == NULL)
-					s = "(null)";
-				while (*s)
-				{
-					putchar(*s);
-					s++;
-					printed_chars++;
-				}
-			}
-			else if (*format == '%')
-			{
-				putchar('%');
-				printed_chars++;
-			}
-		}
-		format++;
-	}
-	va_end(list);
-	return (printed_chars);
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            putchar(*format);
+            printed_chars++;
+        }
+        else
+        {
+            format++;
+            if (*format == 'c')
+            {
+                char c = va_arg(list, int);
+                putchar(c);
+                printed_chars++;
+            }
+            else if (*format == 's')
+            {
+                char *s = va_arg(list, char *);
+                printed_chars += _print_string(s);
+            }
+            else if (*format == '%')
+            {
+                putchar('%');
+                printed_chars++;
+            }
+        }
+        format++;
+    }
+
+    va_end(list);
+
+    return printed_chars;
 }
