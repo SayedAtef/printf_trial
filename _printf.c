3a9aaf22bff9860b359c4da8423b128bb14f22ac
sayed atef <sayedatef6293@gmail.com>
2023-04-24 12:42:09 +0000

    modified printf

--- _printf.c
+++ _printf.c
@@ -7,52 +7,50 @@
  */
 int _printf(const char *format, ...)
 {
-    int printed_chars = 0;
-    va_list list;
+	int printed_chars = 0;
+	va_list list;
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
+	va_start(list, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			putchar(*format);
+			printed_chars++;
+		}
+		else
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(list, int);
 
-    va_start(list, format);
+				putchar(c);
+				printed_chars++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(list, char *);
 
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            putchar(*format);
-            printed_chars++;
-        }
-        else
-        {
-            format++;
-            if (*format == 'c')
-            {
-                char c = va_arg(list, int);
-                putchar(c);
-                printed_chars++;
-            }
-            else if (*format == 's')
-            {
-                char *s = va_arg(list, char *);
-                if (s == NULL)
-                    s = "(null)";
-                while (*s)
-                {
-                    putchar(*s);
-                    s++;
-                    printed_chars++;
-                }
-            }
-            else if (*format == '%')
-            {
-                putchar('%');
-                printed_chars++;
-            }
-        }
-        format++;
-    }
-
-    va_end(list);
-
-    return (printed_chars);
+				if (s == NULL)
+					s = "(null)";
+				while (*s)
+				{
+					putchar(*s);
+					s++;
+					printed_chars++;
+				}
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				printed_chars++;
+			}
+		}
+		format++;
+	}
+	va_end(list);
+	return (printed_chars);
 }
