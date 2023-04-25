0eafdcb9e49b5e62e5da41b9d5ebcac19f21c905
sayed atef <sayedatef6293@gmail.com>
2023-04-25 08:23:46 +0000

    another try on handle print

--- handle_print.c
+++ handle_print.c
@@ -1,27 +1,23 @@
 #include "main.h"
+
 /**
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
- * @ind: ind.
+ * @ind: Pointer to current index in format string.
  * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: 1 or 2;
+ *
+ * Return: The number of characters printed, or -1 on failure.
  */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-	int flags, int width, int precision, int size)
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[])
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
-		{'i', print_int}, {'d', print_int}, {'\0', NULL}
-	};
+		{'i', print_int}, {'d', print_int}, {'\0', NULL}};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+			return (fmt_types[i].fn(list, buffer));
 
 	if (fmt_types[i].fmt == '\0')
 	{
@@ -30,7 +26,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
 			unknow_len += write(1, " ", 1);
-		else if (width)
+		else
 		{
 			--(*ind);
 			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
