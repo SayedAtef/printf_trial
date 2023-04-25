19e487938122ea53c8e05d2f7a244038fb1b531b
sayed atef <sayedatef6293@gmail.com>
2023-04-25 18:41:21 +0000

    print_char

--- print_functions.c
+++ print_functions.c
@@ -13,11 +13,34 @@
  * Return: Number of chars printed
  */
 int print_char(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+    int flags, int width, int precision, int size)
 {
 	char c = va_arg(types, int);
+	int i = 0;
+	char padd = ' ';
+	UNUSED(precision);
+	UNUSED(size);
+	if (flags & F_ZERO)
+		padd = '0';
 
-	return (handle_write_char(c, buffer, flags, width, precision, size));
+	buffer[i++] = c;
+	buffer[i] = '\0';
+
+	if (width > 1)
+	{
+		buffer[BUFF_SIZE - 1] = '\0';
+		for (i = 0; i < width - 1; i++)
+			buffer[BUFF_SIZE - i - 2] = padd;
+
+		if (flags & F_MINUS)
+			return (write(1, &buffer[0], 1) +
+					write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
+		else
+			return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
+					write(1, &buffer[0], 1));
+	}
+
+	return (write(1, &buffer[0], 1));
 }
 /************************* PRINT A STRING *************************/
 /**
